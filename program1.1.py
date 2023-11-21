2f9b0e0d59112ea408fe67d2f15bc8c91689e779
viankahmercado <angelviankahmercado@gmail.com>
2023-11-22 02:08:11 +0800

    First Commit

---
+++ program1.1.py
@@ -0,0 +1,21 @@
+""" Program #1
+
+Write a program that will print the following:
+
+Hi, my name is ______.
+I want to be a _______.
+
+ex.
+Hi, my name is Danilo Madrigalejos.
+I want to be a software engineer. """
+
+# first method
+print("Hi, my name is Angel Viankah Mercado.")
+print("I want to be a data engineer.\n")
+
+# second method
+name = "Hi, my name is Angel Viankah Mercado."
+dream = "I want to be a data engineer."
+
+print(name)
+print(dream)
---
+++ program1.2.py
@@ -0,0 +1,13 @@
+""" Program #1
+
+Write a program that will print the following:
+
+Hi, my name is ______.
+I want to be a _______.
+
+ex.
+Hi, my name is Danilo Madrigalejos.
+I want to be a software engineer. """
+
+str(input("Hi, my name is "))
+str(input("I want to be a "))
